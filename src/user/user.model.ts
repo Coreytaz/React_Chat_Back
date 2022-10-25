5cee4cab04d7998b7ca2b89fcc1c5a7d2ba1dbab
Coreytaz <coreytaz321@gmail.com>
2022-10-25 19:06:29 +0500

    default avatar

--- src/user/user.model.ts
+++ src/user/user.model.ts
@@ -11,7 +11,7 @@ export class UserModel extends TimeStamps {
     @prop({unique: true})
     login: string
 
-    @prop({default: ''})
+    @prop({default: 'http://localhost:5000/api/user/avatars/default.jpg'})
     avatar: string
 
     @prop()
