f198808246802ef671202801956dfcb994ccd1a7
Coreytaz <coreytaz321@gmail.com>
2023-01-15 21:28:05 +0500

    fix

--- src/config/mongo.config.ts
+++ src/config/mongo.config.ts
@@ -4,8 +4,5 @@ import { TypegooseModuleOptions } from 'nestjs-typegoose';
 export const getMongoConfig = async (
   configService: ConfigService,
 ): Promise<TypegooseModuleOptions> => ({
-  uri: configService.get('MONGO_URL'),
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
+  uri: configService.get('MONGO_URL')
 });
