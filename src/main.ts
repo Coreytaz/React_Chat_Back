570d53dd91505f279c8953155e4c34686c01c8b6
Coreytaz <coreytaz321@gmail.com>
2023-01-20 12:10:48 +0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,14 @@ async function bootstrap() {
   try {
     const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule);
+    app.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+      next();
+    });
     app.enableCors({
+      allowedHeaders: '*',
       origin: true,
       methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
       preflightContinue: false,
