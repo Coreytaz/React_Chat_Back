18df9d9ff3c5536e019319e1178c777fb39e1e9b
Coreytaz <coreytaz321@gmail.com>
2023-01-22 20:44:51 +0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   try {
     const PORT = process.env.PORT || 5000;
-    const app = await NestFactory.create(AppModule, { cors: true });
-    app.use((req, res, next) => {
-      res.setHeader('Access-Control-Allow-Credentials', true)
-      res.setHeader('Access-Control-Allow-Origin', '*')
-      res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-      res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-      if (req.method === 'OPTIONS') {
-        res.status(200).end()
-        return
-      }
-      next();
+    const app = await NestFactory.create(AppModule);
+    app.enableCors({
+      origin: 'https://react-web-chat.vercel.app',
+      credentials: true,
     });
     app.setGlobalPrefix('api');
     // app.use(cookieParser());
