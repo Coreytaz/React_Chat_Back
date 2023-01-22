3f5a5565a64c0efec193166357f91560d5e93db5
Coreytaz <coreytaz321@gmail.com>
2023-01-22 20:20:35 +0500

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,20 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   try {
     const PORT = process.env.PORT || 5000;
-    const app = await NestFactory.create(AppModule);
-    app.enableCors({
-      origin: true,
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-      preflightContinue: false,
-      optionsSuccessStatus: 204,
-      credentials: true,
+    const app = await NestFactory.create(AppModule, { cors: true });
+    app.use((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Credentials', true)
+      res.setHeader('Access-Control-Allow-Origin', '*')
+      res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+      res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+      if (req.method === 'OPTIONS') {
+        res.status(200).end()
+        return
+      }
+      next();
     });
     app.setGlobalPrefix('api');
     // app.use(cookieParser());
