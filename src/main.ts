6422728dc233fc1724ed713f9aac3ffa37f574bd
Coreytaz <coreytaz321@gmail.com>
2023-01-22 20:58:20 +0500

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,16 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   try {
     const PORT = process.env.PORT || 5000;
-    const app = await NestFactory.create(AppModule);
-    app.enableCors({
-      origin: 'https://react-web-chat.vercel.app',
-      credentials: true,
+    const app = await NestFactory.create(AppModule, { cors: true });
+    app.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+      if (req.method === 'OPTIONS') {
+        res.status(200).end()
+        return
+      }
+      next();
     });
     app.setGlobalPrefix('api');
     // app.use(cookieParser());
