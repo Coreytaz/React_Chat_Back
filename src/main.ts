869e6e09e1e79b1a8ca30e297303ab6d97e18fc8
Coreytaz <coreytaz321@gmail.com>
2023-01-22 21:16:16 +0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -2,30 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 // import * as cookieParser from 'cookie-parser';
 
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
-
 async function bootstrap() {
   try {
     const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule);
-    app.use(allowCors);
     app.enableCors({
       origin: true,
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+      credentials: true,
     });
     app.setGlobalPrefix('api');
     // app.use(cookieParser());
