0e053a10a1fb3df1bb41d7a3515ccc85240939e8
Coreytaz <coreytaz321@gmail.com>
2023-01-23 23:04:38 +0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import * as cookieParser from 'cookie-parser';
+import cookieParser = require("cookie-parser")
+
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin)
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+}
 
 async function bootstrap() {
   try {
@@ -14,8 +34,8 @@ async function bootstrap() {
       credentials: true,
     });
     app.setGlobalPrefix('api');
-    // app.use(cookieParser());
-
+    app.use(cookieParser());
+    allowCors(handler);
     await app.listen(PORT, () => console.log(`server started on PORT ${PORT}`));
   } catch (error) {
     console.log(error);
