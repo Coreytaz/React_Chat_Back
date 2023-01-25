c963402da43b31edcc47d01b6e8e3850059e182d
Coreytaz <coreytaz321@gmail.com>
2023-01-25 20:31:36 +0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -2,39 +2,27 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser = require("cookie-parser")
 
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', 'http://192.168.0.100:3000')
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
-
-const handler = (req, res) => {
-  const d = new Date()
-  res.end(d.toString())
-}
-
 async function bootstrap() {
   try {
     const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule);
     app.enableCors({
-      origin: 'http://192.168.0.100:3000',
+      origin: 'https://react-web-chat.vercel.app',
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
       credentials: true,
       allowedHeaders: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
     });
     app.setGlobalPrefix('api');
     app.use(cookieParser());
-    allowCors(handler);
+    app.use(
+      (req, res, next) => {
+        if (req.method === 'OPTIONS') {
+          res.status(200).end()
+          return
+        }
+        next()
+      }
+    )
     await app.listen(PORT, () => console.log(`server started on PORT ${PORT}`));
   } catch (error) {
     console.log(error);
